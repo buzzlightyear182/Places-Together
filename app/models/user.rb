9d569e783115af92b4f6b6d2cbe9d64d76565d5e
buzzlightyear182 <jane.buzzlightyear@gmail.com>
2014-08-20 21:26:50 +0200

    Clean-up test specs and fixed devise sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ActiveRecord::Base
 
 	validates :username, presence: true, length: {maximum: 20}, format: {with: /\A[a-zA-Z0-9]+\z/}
 	validates :encrypted_password, presence: true
-	validates :email, presence: true, format: {with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/}
+	validates :email, presence: true, format: {with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/}, uniqueness: true
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,22 +12,33 @@ user3 = User.create username: "kristiankiwi", fullname:"Kristian Kyvik", passwor
 user4 = User.create username: "bentarenne", fullname:"Ben Tarenne", password: '12345678', email: "bentarenne@gmail.com"
 user5 = User.create username: "cat1788", fullname:"Cat Burston", password: '12345678', email: "cat1788@gmail.com"
 user6 = User.create username: "rogerbriz", fullname:"Roger Briz", password: '12345678', email: "rogerbriz@gmail.com"
+user7 = User.create username: "mentor", fullname: "Wojtek Ogrodowczyk", password: '12345678', email: "wojtek.ogrodowczyk@gmail.com"
 
 place1 = Place.create city: 'Manila', country: 'Philippines'
 place2 = Place.create city: 'Montpellier', country: 'France'
 place3 = Place.create city: 'Barcelona', country: 'Spain'
 place4 = Place.create city: 'Bilbao', country: 'Spain'
 place5 = Place.create city: 'Oslo', country: 'Norway'
+place6 = Place.create city: 'Stockholm', country: 'Sweden'
 
 activity1 = Activity.create activity_name: "Diving", category: "Water Sport"
 activity2 = Activity.create activity_name: "Hackathon", category: "Indoor"
 activity3 = Activity.create activity_name: "Fiesta", category: "Nightlife"
 activity4 = Activity.create activity_name: "Trek", category: "Outdoor"
+activity5 = Activity.create activity_name: "Tourism", category: "Leisure"
 
-Trip.create organizer: user1.id, place_id: place1.id, activity_id: activity1.id, from_date: Date.today+30, to_date: Date.today+32, description: "Wreck dive", capacity: 10
+trip1 = Trip.create organizer: user1.id, place_id: place1.id, activity_id: activity1.id, from_date: Date.today+30, to_date: Date.today+32, description: "Wreck dive", capacity: 10
 
-Trip.create organizer: user2.id, place_id: place3.id, activity_id: activity2.id, from_date: Date.today+1, to_date: Date.today+2, description: "Code kata", capacity: 5
+trip2 = Trip.create organizer: user2.id, place_id: place3.id, activity_id: activity2.id, from_date: Date.today+1, to_date: Date.today+2, description: "Code kata", capacity: 5
 
-Trip.create organizer: user3.id, place_id: place5.id, activity_id: activity3.id, from_date: Date.today+2, to_date: Date.today+3, description: "Night out", capacity: 8
+trip3 = Trip.create organizer: user3.id, place_id: place5.id, activity_id: activity3.id, from_date: Date.today+2, to_date: Date.today+3, description: "Night out", capacity: 8
 
-Trip.create organizer: user6.id, place_id: place4.id, activity_id: activity4.id, from_date: Date.today+6, to_date: Date.today+9, description: "Climb mountains", capacity: 4
+trip4 = Trip.create organizer: user6.id, place_id: place4.id, activity_id: activity4.id, from_date: Date.today+6, to_date: Date.today+9, description: "Climb mountains", capacity: 4
+
+trip5 = Trip.create organizer: user4.id, place_id: place2.id, activity_id: activity5.id, from_date: Date.today+4, to_date: Date.today+7, description: "Eat french cheese", capacity: 3
+
+Participation.create user_id: user2.id, trip_id: trip1.id, confirmed: false
+Participation.create user_id: user7.id, trip_id: trip2.id, confirmed: false
+Participation.create user_id: user1.id, trip_id: trip3.id, confirmed: false
+Participation.create user_id: user5.id, trip_id: trip4.id, confirmed: false
+Participation.create user_id: user3.id, trip_id: trip5.id, confirmed: false
