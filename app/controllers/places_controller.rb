81cad18885dc4740c295560c3a169884848bd3a5
buzzlightyear182 <jane.buzzlightyear@gmail.com>
2014-08-24 17:24:43 +0200

    Heroku test2

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,6 +3,9 @@ class PlacesController < ApplicationController
 	def index
 		@places = Place.all
 		@activities = Activity.all
+    if current_user
+      @profile_id = Profile.where(user_id: current_user.id).first.user_id
+    end
 	end
 
 end
