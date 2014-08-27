966fc3de259af8ddea464ef20a175e08fdab57f3
buzzlightyear182 <jane.buzzlightyear@gmail.com>
2014-08-27 10:00:02 +0200

    Refactor search

--- app/models/search.rb
+++ app/models/search.rb
@@ -12,16 +12,21 @@ class Search
   end
 
   def search(keyword)
-    place_result = Place.where(city: keyword).first
-    activity_result = Activity.where(activity_name: keyword).first
+    place_result = Place.where('city ILIKE ?', "%#{keyword}%")
+    activity_result = Activity.where('activity_name ILIKE ?', "%#{keyword}%")
 
-    if place_result != nil
-      @search_results = Trip.where(place_id: place_result.id).all
-    elsif activity_result != nil
-      @search_results = Trip.where(activity_id: activity_result.id).all
+    if place_result.size != 0
+      place_result.each do |place|
+        @search_results = Trip.where(place_id: place.id).all
+      end
+    elsif activity_result.size != 0
+      activity_result.each do |activity|
+        @search_results = Trip.where(activity_id: activity_result.id).all
+      end
     end
 
     @search_results
+
   end
 
 end
