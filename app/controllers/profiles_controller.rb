fb036f101e7cb781e3c7079d8388ee40379fc972
buzzlightyear182 <jane.buzzlightyear@gmail.com>
2014-08-29 09:13:32 +0200

    Style reviews new

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -33,7 +33,7 @@ class ProfilesController < ApplicationController
 private
 
   def profile_params
-    params.require(:profile).permit(:first_name, :surname, :hometown, :birthday, :occupation, :avatar, :languages, :facebook, :twitter, :instagram, :google, :blog)
+    params.require(:profile).permit(:first_name, :surname, :hometown, :birthday, :occupation, :languages, :facebook, :twitter, :instagram, :google, :blog)
   end
 
 end
